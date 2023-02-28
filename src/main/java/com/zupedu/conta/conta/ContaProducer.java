4b869d1225ffd984e7f86b63efd0a1fa04eb5ced
Gian Marcel de Souza <gianmarcelsouza@gmail.com>
2023-02-28 11:09:11 -0300

    include kafka configs

--- src/main/java/com/zupedu/conta/conta/ContaProducer.java
+++ src/main/java/com/zupedu/conta/conta/ContaProducer.java
@@ -1,6 +1,7 @@
 package com.zupedu.conta.conta;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,11 @@ public class ContaProducer {
     @Autowired
     private KafkaTemplate<String, Conta> kafkaTemplate;
 
+    @Value("${spring.kafka.producer.topic-name}")
+    private String topicName;
+
     @Async
-    public void sendKafka(Conta conta){
-        kafkaTemplate.send("NOVA_CONTA", conta);
+    public void sendKafka(Conta conta) {
+        kafkaTemplate.send(topicName, conta);
     }
 }
